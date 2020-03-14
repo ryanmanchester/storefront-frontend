11bf2d6f27f327c4a7d85811e9d396a5f94fe7b6
Ryan Manchester <ryanm17@gmail.com>
2020-03-14 15:18:44 -0400

    working on getting categories from api

---
+++ src/actions/categoryActions.js
@@ -0,0 +1,7 @@
+export const fetchCategories = () => {
+  return (dispatch) => {
+    fetch('http://localhost:3000/api/v1/categories')
+    .then(resp => resp.json())
+    .then(categories => dispatch({type: 'GET_CATEGORIES', categories: categories.name}))
+  }
+}
