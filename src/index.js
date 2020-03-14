8e4cb305d11682cbf8e40ae466493eb36af7e18b
Ryan Manchester <ryanm17@gmail.com>
2020-03-14 14:45:40 -0400

    mock-up nav bar for categories

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import manageCart from './reducers/manageCart.js'
 
-
-const store = createStore(manageCart, composeWithDevTools(applyMiddleware(thunk)))
+const reducer = combineReducers({
+  cart: manageCart
+})
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)))
 
 
 ReactDOM.render(<Provider store={ store }><App /></Provider>, document.getElementById('root'));
