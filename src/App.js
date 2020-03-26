3e3f56dd92c50e7a532d63b06e403a0e6adaee3c
github username <github email address>
2020-03-25 21:38:51 -0400

    add isLoading to reducer to display category names

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getCategories } from './actions/categoryActions';
+import { connect } from 'react-redux';
 import NavBar from './components/NavBar.js';
+import Categories from './components/Categories';
 
  class App extends Component {
+   componentDidMount = () => {
+     this.props.getCategories()
+   }
   render(){
+    console.log(this.props.categories.categories)
     return (
       <div className="App">
         <nav><NavBar /></nav>
         <header className="App-header">
           <h1>Welcome to StoreFront</h1>
+          <Categories categories={this.props.categories} />
         </header>
       </div>
     )
   }
-
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+   categories: state.categories
+  }
+}
+
+export default connect(mapStateToProps, { getCategories })(App);
--- src/reducers/categoriesReducer.js
+++ src/reducers/categoriesReducer.js
@@ -1,9 +1,16 @@
-const categoriesReducer = (state = [], action) => {
+const categoriesReducer = (state = { categories: [], isLoading: false}, action) => {
   switch (action.type) {
+    case 'LOADING_CATEGORIES':
+      return {
+        ...state,
+        categories: [...state.categories],
+        isLoading: true
+      }
     case 'GET_CATEGORIES':
       return {
         ...state,
-        categories: action.categories
+        categories: action.categories,
+        isLoading: false
       }
 
     default:
