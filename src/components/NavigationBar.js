ccfa5e1de6a4dfecfe0fa6d689cb8945458ebdc1
Ryan Manchester <ryanm17@gmail.com>
2020-03-29 22:30:36 -0400

    category buttons redirecting via react router

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,11 +3,12 @@ import { Navbar, Nav } from 'react-bootstrap';
 
  const NavigationBar = () => {
   return (
-    <Navbar bg="light" variant="light" defaultActiveKey="/">
+    <Navbar sticky="top" bg="light" variant="light">
       <Navbar.Brand>StoreFront</Navbar.Brand>
       <Navbar.Collapse className="justify-content-end">
           <Nav.Link  href="/">Home</Nav.Link>
           <Nav.Link href="/signup">Become a Seller</Nav.Link>
+          <Nav.Link href="/login">Log In </Nav.Link>
           <Nav.Link href="/cart">Cart</Nav.Link>
       </Navbar.Collapse>
 
