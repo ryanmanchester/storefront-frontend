501e59fadbbf5f5fc37af853a3b2d350c196d57c
github username <github email address>
2020-05-17 19:41:46 -0400

    saves session as cookies so seller stays logged in

--- src/actions/currentSeller.js
+++ src/actions/currentSeller.js
@@ -9,11 +9,34 @@ export const login = (creds) => {
   console.log('creds are', creds)
   return dispatch => {
     return fetch('http://localhost:3000/api/v1/sellers/login', {
+      credentials: 'include',
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(creds)
+    })
+    .then(resp => resp.json())
+    .then(seller => {
+      if (seller.error) {
+        alert(seller.error)
+      } else {
+        dispatch(setCurrentSeller(seller))
+      }
+    })
+  }
+}
+
+export const getCurrentSeller = () => {
+
+  return dispatch => {
+    return fetch('http://localhost:3000/api/v1/sellers/get_current_seller', {
+      credentials: 'include',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+
     })
     .then(resp => resp.json())
     .then(seller => {
