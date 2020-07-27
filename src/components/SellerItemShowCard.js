32ad234956855c73c360e392906e59a40578975a
Ryan Manchester <ryanm17@gmail.com>
2020-07-27 18:48:02 -0400

    styles the seller item show page

--- src/components/SellerItemShowCard.js
+++ src/components/SellerItemShowCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Card } from 'react-bootstrap';
+import { Card, Container } from 'react-bootstrap';
 
 const SellerItemShowCard = ({items, match}) => {
 console.log(items)
@@ -8,15 +8,20 @@ if (items) {
   const currentItem = items.find(item => item.id === match.params.id)
   console.log(currentItem)
   return (
-        <Card className="bg-dark text-white">
-      <Card.Img src={currentItem.attributes.image_url} alt="Card image" />
-      <Card.ImgOverlay>
-        <Card.Title>{currentItem.attributes.title}</Card.Title>
-        <Card.Text>
-          {currentItem.attributes.description}
-        </Card.Text>
-      </Card.ImgOverlay>
-    </Card>
+    <Container style={ {width: "36rem"} }>
+    <h1>{currentItem.attributes.name}</h1>
+    <Card>
+      <Card.Img variant="top" src={currentItem.attributes.image_url} style={ {width: '100%',
+                                                            height: '36rem',
+                                                            objectFit: 'cover'} } />
+      <Card.Body>
+       <Card.Title>{currentItem.attributes.description}</Card.Title>
+       <Card.Subtitle>{currentItem.attributes.sold ? "Sold Out" : `$${currentItem.attributes.price}`}</Card.Subtitle>
+       <Card.Link href="#">Edit Item</Card.Link>
+       <Card.Link href="#">Delete Item</Card.Link>
+     </Card.Body>
+     </Card>
+    </Container>
   )
 } else {
   return ("Loading...")
