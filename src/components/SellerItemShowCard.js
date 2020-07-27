70fa4f5e3c85169a6f953a38219bb087f385c67f
github username <github email address>
2020-07-27 18:02:27 -0400

    links sellers' item index page to show page

--- src/components/SellerItemShowCard.js
+++ src/components/SellerItemShowCard.js
@@ -1,20 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Card } from 'react-bootstrap';
 
-const SellerItemShowCard = ({item}) => {
-  console.log(item)
+const SellerItemShowCard = ({items, match}) => {
+console.log(items)
+if (items) {
+  const currentItem = items.find(item => item.id === match.params.id)
+  console.log(currentItem)
   return (
-    <div>Sellers Item Show Page</div>
-//     <Card className="bg-dark text-white">
-//   <Card.Img src={item.image_url} alt="Card image" />
-//   <Card.ImgOverlay>
-//     <Card.Title>{item.title}</Card.Title>
-//     <Card.Text>
-//       {item.description}
-//     </Card.Text>
-//   </Card.ImgOverlay>
-// </Card>
+        <Card className="bg-dark text-white">
+      <Card.Img src={currentItem.attributes.image_url} alt="Card image" />
+      <Card.ImgOverlay>
+        <Card.Title>{currentItem.attributes.title}</Card.Title>
+        <Card.Text>
+          {currentItem.attributes.description}
+        </Card.Text>
+      </Card.ImgOverlay>
+    </Card>
   )
+} else {
+  return ("Loading...")
 }
 
-export default SellerItemShowCard
+
+}
+
+const mapStateToProps = state => {
+  return {
+    items: state.currentSeller.included
+  }
+}
+
+export default connect(mapStateToProps)(SellerItemShowCard)
