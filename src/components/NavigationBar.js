dd035f87695bd7a5eb06aa0a327155e220999133
github username <github email address>
2020-10-15 10:19:22 -0400

    changes navlink to react router link. does not redirect/reload page

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -11,9 +11,9 @@ import { logout } from '../actions/currentSeller';
       <Navbar sticky="top" bg="light" variant="light">
         <Navbar.Brand>StoreFront</Navbar.Brand>
         <Navbar.Collapse className="justify-content-end">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href={`/sellers/${currentSeller.data.id}/items`}>{currentSeller.data.attributes.name}s Shop</Nav.Link>
-            <Nav.Link href="/cart">Cart</Nav.Link>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to={`/sellers/${currentSeller.data.id}/items`}>{currentSeller.data.attributes.name}s Shop</NavLink>
+            <NavLink to="/cart">Cart</NavLink>
         </Navbar.Collapse>
 
       </Navbar>
