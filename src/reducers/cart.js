e4f8c13ecc29436d5bd9fc640de94a336f14125c
github username <github email address>
2020-11-14 22:52:20 -0500

    adds button disable after item added to cart

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,21 +1,22 @@
 
-export default (state = { items: [] }, action) => {
+export default (state = { items: []}, action) => {
   switch(action.type) {
     case "ADD_TO_CART":
     const addItem = state.items.concat(action.item)
       return {
         ...state,
-        items: addItem
+        items: addItem,
+
       }
       case "REMOVE_ITEM":
       const itemsList = state.items.filter(item => item.id !== action.item.id)
       return {
         ...state,
-        items: itemsList
+        items: itemsList,
       }
       case "CLEAR_CART":
       return {
-        items: []
+        items: [],
       }
     default:
       return state
