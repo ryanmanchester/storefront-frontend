3f3f9bceaea3500cf97c041bef8d95b7e65fd66f
github username <ryanm17@gmail.com>
2021-03-03 12:23:46 -0500

    build with create react app on heroku

--- src/actions/accessoryActions.js
+++ src/actions/accessoryActions.js
@@ -12,7 +12,7 @@ export const setAccessories = items => {
 export const getAccessories = () => {
   return (dispatch) => {
     dispatch({type: 'LOADING_ACCESSORIES'})
-    fetch('http://localhost:3000/api/v1/categories/3/items')
+    fetch('https://storefront-backend.herokuapp.com/api/v1/categories/3/items')
     .then(resp => resp.json())
     .then(items => dispatch(setAccessories(items.data)))
     .catch(console.log)
